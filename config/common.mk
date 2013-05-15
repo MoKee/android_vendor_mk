35a7b100f735cc55661d469c41b9d024e6c1df09
martincz <martincz@mokeetek.com>
2013-05-15 19:42:36 +0800

    vendor/mk: script fix
    Change-Id: I54d8ea4de6c3af7242d59c10a800cac04d76a9a7

--- config/common.mk
+++ config/common.mk
@@ -96,7 +96,7 @@ PRODUCT_COPY_FILES += \
     vendor/mk/prebuilt/common/app/MoKeeMarket.apk:system/app/MoKeeMarket.apk \
     vendor/mk/prebuilt/common/lib/libkpencore.so:system/lib/libkpencore.so \
     vendor/mk/prebuilt/common/lib/librabjni-1.so:system/lib/librabjni-1.so \
-    vendor/mk/prebuilt/common/lib/libshare.so:system/lib/libshare.so
+    vendor/mk/prebuilt/common/lib/libshare_v2.so:system/lib/libshare_v2.so
 
 # Compcache/Zram support
 PRODUCT_COPY_FILES += \
