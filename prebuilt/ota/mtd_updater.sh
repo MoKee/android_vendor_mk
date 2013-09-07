0e31bd67dcf2a6c1808cc0aae08f47e549887d6f
Ryuinferno <ryuinferno.xda@gmail.com>
2013-09-07 22:01:40 +1000

    OTA: Move prebuilt stuff to vendor (2/2)
    Change-Id: I00d53efe410ac346263500990a8496cbb2eb3e04

---
+++ prebuilt/ota/mtd_updater.sh
@@ -0,0 +1,29 @@
+#!/sbin/sh
+#
+# Copyright (C) 2013 The MoKee OpenSource Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Check if we're running on a bml, mtd (old) or mtd (current) device
+if /sbin/busybox test -e /dev/block/bml7 ; then
+	# We're running on a bml device
+	/tmp/flash_image boot /tmp/boot.img
+elif /sbin/busybox test `/sbin/busybox cat /sys/class/mtd/mtd2/size` != "$MTD_SIZE" || \
+    /sbin/busybox test `/sbin/busybox cat /sys/class/mtd/mtd2/name` != "datadata" ; then
+	# We're running on a mtd (old) device
+	/tmp/bml_over_mtd.sh boot 72 reservoir 2004 /tmp/boot.img
+elif /tmp/busybox test -e /dev/block/mtdblock0 ; then
+	# We're running on a mtd (current) device
+	/tmp/bml_over_mtd.sh boot 72 reservoir 2004 /tmp/boot.img
+fi
