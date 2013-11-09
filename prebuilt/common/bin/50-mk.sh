e0a82191fb6864fb2804224919528fc4a351cc66
martincz <martincz@mokeetek.com>
2013-11-10 01:59:12 +0800

    update
    Change-Id: I63abcaac91bbadc6dd309488a9bb31692834fd9a

--- prebuilt/common/bin/50-mk.sh
+++ prebuilt/common/bin/50-mk.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 # 
 # /system/addon.d/50-mk.sh
-# During a MK43 upgrade, this script backs up /system/etc/hosts,
+# During a MK44 upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
