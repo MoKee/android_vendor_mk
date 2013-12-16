9190955ba1765351e4848240a9f6abebab8f5fdd
Ryuinferno <ryuinferno.xda@gmail.com>
2013-12-16 19:41:57 +0100

    vendor: Use our own sound configuration
    Change-Id: Id3dd9e2019b4c9f7c37e641fd3c0fa72c80481cf

--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -1,17 +1,14 @@
 # Inherit common MK stuff
 $(call inherit-product, vendor/mk/config/common.mk)
 
-# Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
 # Include MK audio files
 include vendor/mk/config/mk_audio.mk
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Orion.ogg \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+    ro.config.notification_sound=Alya.ogg \
+    ro.config.alarm_alert=Alarm_Buzzer.ogg
 
 # BT config
 PRODUCT_COPY_FILES += \
