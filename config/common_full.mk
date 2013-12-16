9a5bf2a126f44620d90915425ba25c6479688a74
Ryuinferno <ryuinferno.xda@gmail.com>
2013-12-16 19:41:57 +0100

    vendor: Use our own sound configuration
    Change-Id: Id3dd9e2019b4c9f7c37e641fd3c0fa72c80481cf

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,15 +1,6 @@
 #Inherit common MK stuff
 $(call inherit-product, vendor/mk/config/common.mk)
 
-# Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
-# Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
-
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Include MK audio files
 include vendor/mk/config/mk_audio.mk
 
