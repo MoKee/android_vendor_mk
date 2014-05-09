ae45220385413d9f1456875d7c01a80c6ec8d70a
martincz <martincz@mokeedev.com>
2014-05-09 20:31:01 +0800

    fix compile
    Change-Id: I245266cd8d9c37869d7235dfe94d4f836dd395a5

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -4,5 +4,5 @@ PRODUCT_PACKAGES += \
     ThemesProvider
 
 PRODUCT_COPY_FILES += \
-    vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-    vendor/cm/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+    vendor/mk/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/mk/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
