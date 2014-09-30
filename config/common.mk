6d33ecdea2bd8dd920ed7c40854ddc587cd89b84
Ryuinferno <ryuinferno.xda@gmail.com>
2014-10-01 01:42:21 +0200

    vendor: Add chromium prebuilt support
    export USE_PREBUILT_CHROMIUM=1 to enable, reduces build time around 27%
    
    Change-Id: I679fef57035132944dd9d50ca698878b10200c88

--- config/common.mk
+++ config/common.mk
@@ -148,6 +148,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# Chromium Prebuilt
+ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
+-include prebuilts/chromium/$(TARGET_DEVICE)/chromium_prebuilt.mk
+endif
+
 # This is MK!
 PRODUCT_COPY_FILES += \
     vendor/mk/config/permissions/com.mokee.android.xml:system/etc/permissions/com.mokee.android.xml
