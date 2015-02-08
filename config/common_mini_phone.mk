3928b817c63a5e08b62beb779ac199fbab8d515d
martincz <martincz@mokeedev.com>
2015-02-09 00:31:33 +0800

    vendor/mk: update media files
    Change-Id: I2c9ffc55dc1e4bca803cd1fa682c4b749f89f75c

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -7,7 +7,7 @@ include vendor/mk/config/mk_audio.mk
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Tethys.ogg \
-    ro.config.alarm_alert=Oxygen.ogg
+    ro.config.alarm_alert=Hello.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
