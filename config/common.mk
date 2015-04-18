5f2c89890b570c7bfc899d0cf20cb272e651734e
martincz <martincz@mokeedev.com>
2015-04-18 16:13:54 +0800

    mk: add MoKeePay and exclude adb authentication for experimental
    Change-Id: Iaf97b884afb33044a19b78c53143124ed0807d65

--- config/common.mk
+++ config/common.mk
@@ -62,8 +62,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
-# Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+  ifdef MK_EXPERIMENTAL
+    # Disable ADB authentication
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+  endif
+  else
+    # Enable ADB authentication
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+  endif
 endif
 
 # Copy over the translator to the device
@@ -184,6 +190,7 @@ PRODUCT_PACKAGES += \
     Eleven \
     LockClock \
     MoKeeHelper \
+    MoKeePay \
     MoKeeSetupWizard \
 
 # MK Hardware Abstraction Framework
