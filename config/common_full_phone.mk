b17d199d39ff429abbe42a68a9bb30b1064eef1d
martincz <martincz@mokeedev.com>
2015-04-29 15:09:23 +0800

    config: update alarms
    Change-Id: Iaf43141d382f64fc7d4911958baf157e2b9d31de

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/mk/config/common_full.mk)
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Hello.ogg \
-    ro.config.alarm_alert=Helium.ogg
+    ro.config.alarm_alert=angels_harp.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
