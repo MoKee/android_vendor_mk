7891ec879bdc51905a6b67614eb4ee52dd0a8567
NoelMacwan <yulespencer@gmail.com>
2015-06-30 18:44:32 +0700

    mk: Fix prebuilt lib script
    For 64bit devices, the libs were getting copied to /system/lib instead of /system/lib64.
    which was causing Setting FC
    
    PS1: iprove script
    
    Change-Id: I6f5c1b13f8d43e4114ca729f65750fa33bce1047
    Signed-off-by: NoelMacwan <yulespencer@gmail.com>

--- config/mk_prebuilt.mk
+++ config/mk_prebuilt.mk
@@ -3,8 +3,13 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/mk/prebuilt/private/bin/$(MK_CPU_ABI),system/bin)
 
 # Use all private libraries
+ifeq ($(MK_CPU_ABI),arm64-v8a)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.so,vendor/mk/prebuilt/private/lib/$(MK_CPU_ABI),system/lib64)
+else
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*.so,vendor/mk/prebuilt/private/lib/$(MK_CPU_ABI),system/lib)
+endif
 
 # Use all third-party files
 PRODUCT_COPY_FILES += \
