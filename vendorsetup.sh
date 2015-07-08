de9418ce211920d66ea89f677b9cc581d11a9868
martincz <martincz@mokeedev.com>
2015-07-09 00:11:39 +0800

    better printf format
    Change-Id: I6411b14a442a4360974ab22a6ee8ff0a1836de94

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/MoKee/android_external_mokee_BuildController/mkl-mr1/mk_build_targets | sed -e 's/#.*$//' | awk {'print $1'})
+for combo in $(curl -s https://raw.githubusercontent.com/MoKee/android_external_mokee_BuildController/mkl-mr1/mk_build_targets | sed -e 's/#.*$//' | awk '{printf "mk_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
