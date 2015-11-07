4710763dbcdf6bd8d3c0d39fe13cd9e0067bc779
martincz <martincz@mokeedev.com>
2015-11-07 20:42:43 +0800

    mk: disable block option
    Change-Id: I7b6aef71eaf20020d43a250ef5319d3dc09f28ec

--- config/mk_extra.mk
+++ config/mk_extra.mk
@@ -4,4 +4,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 MOKEEHELPER_EMBEDDED := true
 MOKEEHELPER_PACKAGE_PREFIX := com.android.settings.mokee.mokeehelper
 
+BLOCK_BASED_OTA := false
+
 -include vendor/mk/config/mk_prebuilt.mk
