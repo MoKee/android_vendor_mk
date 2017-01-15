a9c361f95d827ae06c3aff3addccf717cd37a478
martincz <martincz@mokeedev.com>
2017-01-16 04:28:07 +0800

    mk: default enable SU
    Change-Id: Ib70f0988aff8546d3e1e3a8cc0f8f4b3705a66fd

--- config/mk_extra.mk
+++ config/mk_extra.mk
@@ -2,6 +2,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opa.eligible_device=true \
     persist.radio.ipcall.enabled=true
 
+WITH_SU := true
+
 BLOCK_BASED_OTA := false
 
 -include vendor/private/mk_prebuilt.mk
