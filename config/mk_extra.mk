5248b83b6e3a7105bf1e121295e51281ffb5f376
martincz <martincz@mokeedev.com>
2018-02-04 02:28:27 +0800

    mk: Derp
    Change-Id: I3c0404f1a42baf728b57f4a1af2ede147f736ea4

--- config/mk_extra.mk
+++ config/mk_extra.mk
@@ -1,12 +1,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opa.eligible_device=true
 
-ifdef MK_EXPERIMENTAL
-  BLOCK_BASED_OTA := true
-else
-  BLOCK_BASED_OTA := false
-endif
-
 WITH_SU := true
 
 -include vendor/private/mk_private.mk
