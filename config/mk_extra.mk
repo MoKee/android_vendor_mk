8cd7cc44405d8b12fe2afdd32a706e218c2fc5bc
Martincz Gao <martincz@mokeedev.com>
2018-07-10 11:49:25 +0800

    mk: disable recents grid view
    Change-Id: I331ccc6f99416a95e1c836c72e3afcaae8787185

--- config/mk_extra.mk
+++ config/mk_extra.mk
@@ -1,4 +1,5 @@
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.opa.eligible_device=true
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true \
+    ro.recents.grid=true
 
 -include vendor/private/mk_private.mk
