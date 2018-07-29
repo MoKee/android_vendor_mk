ce116936abf1e8aad91c7c0856e2c15357d48793
Martincz Gao <martincz@mokeedev.com>
2018-07-29 15:01:29 +0800

    Revert "ATV: Build LineageCustomizer"
    This reverts commit 6674179a47622dd3f464e80da94b8ade8fa6f353.

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,8 +1,6 @@
 # Inherit full common MK stuff
 $(call inherit-product, vendor/mk/config/common_full.mk)
 
-PRODUCT_PACKAGES += \
-    AppDrawer \
-    LineageCustomizer
+PRODUCT_PACKAGES += AppDrawer
 
 DEVICE_PACKAGE_OVERLAYS += vendor/mk/overlay/tv
