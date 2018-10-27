0f6a207f860fcdef08f7cc10a8530eeae73eb534
Martincz Gao <martincz@mokeedev.com>
2018-10-27 22:01:26 +0800

    mk: Don't forget captcha Provider list
    Change-Id: I08a88498726d5792866573212af3d6268fc18604
    (cherry picked from commit 0257f1b0408babc3281696e755f336abfb01a523)
    (cherry picked from commit 68394e2d61a5cf6ef300cf4faf07cbe680abe950)

--- config/telephony.mk
+++ config/telephony.mk
@@ -6,6 +6,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/mk/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# Captcha Provider list
+PRODUCT_COPY_FILES += \
+    vendor/mk/prebuilt/common/etc/captcha-provider-conf.xml:system/etc/captcha-provider-conf.xml
+
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
     vendor/mk/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
