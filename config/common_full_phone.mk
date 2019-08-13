a8852271f62f806cab33f5a43ce77dc16a084272
Martincz Gao <martincz@mokeedev.com>
2019-08-13 17:26:51 +0800

    mk: Drop LatinIME dictionaries
    Change-Id: I40166b724422e4c0edfbb27a8d37ba73d9599f8c

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,7 +1,4 @@
 # Inherit full common MoKee stuff
 $(call inherit-product, vendor/mk/config/common_full.mk)
 
-# Include MoKee LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/mk/overlay/dictionaries
-
 $(call inherit-product, vendor/mk/config/telephony.mk)
--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,5 +1,2 @@
 # Inherit full common MoKee stuff
 $(call inherit-product, vendor/mk/config/common_full.mk)
-
-# Include MoKee LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/mk/overlay/dictionaries
