481b550e9a61bc19db40be387d5548e39c9013eb
martincz <martincz@mokeedev.com>
2019-08-15 15:37:00 +0800

    Revert "mk: Drop LatinIME dictionaries"
    This reverts commit a8852271f62f806cab33f5a43ce77dc16a084272.
    
    Change-Id: I72908b7b88fde4d184c1575c2d37e163f66f6243

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,4 +1,7 @@
 # Inherit full common MoKee stuff
 $(call inherit-product, vendor/mk/config/common_full.mk)
 
+# Include MoKee LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/mk/overlay/dictionaries
+
 $(call inherit-product, vendor/mk/config/telephony.mk)
