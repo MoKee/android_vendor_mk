3756195a9df1c4ac0f36bb6d2ee1fe6069298f4f
XiNGRZ <chenxingyu92@gmail.com>
2019-12-24 10:37:13 +0800

    extract_utils: Fix bad substitution for zsh
    Change-Id: I5f4fabc62b1ac3da159b36c3ae6080e145948530

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1177,7 +1177,7 @@ function oat2dex() {
     local SRC="$3"
     local TARGET=
     local OAT=
-    local HOST="$(uname)"
+    local HOST="$(uname | tr '[:upper:]' '[:lower:]')"
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
         export BAKSMALIJAR="$MOKEE_ROOT"/prebuilts/tools-mokee/common/smali/baksmali.jar
@@ -1185,11 +1185,11 @@ function oat2dex() {
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$MOKEE_ROOT"/prebuilts/tools-mokee/"${HOST,,}"-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$MOKEE_ROOT"/prebuilts/tools-mokee/${HOST}-x86/bin/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$MOKEE_ROOT"/prebuilts/tools-mokee/"${HOST,,}"-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$MOKEE_ROOT"/prebuilts/tools-mokee/${HOST}-x86/bin/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
